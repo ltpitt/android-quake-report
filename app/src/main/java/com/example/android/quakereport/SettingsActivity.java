879a732ee34012caaf52f03c4dcf2b54c15d15a9
Davide Nastri <d.nastri@gmail.com>
2021-05-16 21:41:40 +0200

    wip Fixing gradle and updating dependencies and code

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -20,7 +22,7 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.layout.settings_main);
+            //addPreferencesFromResource(R.layout.settings_main);
 
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
